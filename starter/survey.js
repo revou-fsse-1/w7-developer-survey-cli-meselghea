c747d1fbb336fddd86018a7a8c8e5d9a0c3cbe47
meselghea <122159476+meselghea@users.noreply.github.com>
2023-03-07 23:35:31 +0700

    add survey

--- starter/survey.js
+++ starter/survey.js
@@ -1,7 +1,46 @@
 import inquirer from "inquirer";
 
 const questions = [
-  // Type your question here
+  {
+    type: "input",
+    name: "name",
+    message: "What's your first name?"
+},
+{
+    type: "input",
+    name: "email",
+    message: "What's your email address?"
+},
+{
+    type: "list",
+    name: "experience",
+    message: "How many years of experience you have with JavaScript?",
+    choices: [
+        "0-1",
+        "1-3",
+        "3-5",
+        "5-10",
+        "10+"
+    ]
+},
+{
+    type: "checkbox",
+    name: "technologies",
+    message: "What technologies do you know?",
+    choices: [
+        "React.js",
+        "Vue",
+        "Angular",
+        "Node.js",
+        "jQuery",
+        "D3.js",
+    ]
+},
+{
+    type: "number",
+    name: "salary",
+    message: "What is your salary requirement?"
+}
 ];
 
 // run your command
